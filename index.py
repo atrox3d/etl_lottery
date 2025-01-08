ddf7ccb328e3b5b6717da396e514557383518c5b
atrox3d <atrox3d@gmail.com>
2025-01-08 16:28:36 +0100

    create index page

---
+++ index.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+st.title('Analisi lotteria italia')
+
+st.write('''
+        Dashboard per analisi vincite Lotteria Italia 2024 - 2025
+        
+        ''')
+
+st.sidebar.header('Filtri')
+st.sidebar.write('Usare le opzioni per filtrare i dati')
+
+st.sidebar.subheader('Provincia')
+
+st.sidebar.subheader('Luogo')
+
+
