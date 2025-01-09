23a5063e6f3d4ac57ea98502bd0deef67c3eaf76
atrox3d <atrox3d@gmail.com>
2025-01-09 07:34:22 +0100

    simpler import of dal

--- index.py
+++ index.py
@@ -2,13 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-from dashboard.dal import (
-    get_connection_status, 
-    get_winners,
-    get_provence,
-    get_location,
-    get_category
-)
+from dashboard import dal
 
 
 st.title('Analisi lotteria italia')
@@ -16,7 +10,7 @@ st.title('Analisi lotteria italia')
 st.write(f'''
         Dashboard per analisi vincite Lotteria Italia 2024 - 2025
         
-        stato della connessione: {get_connection_status()}
+        stato della connessione: {dal.get_connection_status()}
         ''')
 
 
@@ -27,28 +21,28 @@ st.sidebar.subheader('Geograficamente')
 provence = st.sidebar.selectbox(
     label='seleziona una provincia',
     index=None,
-    options=get_provence()
+    options=dal.get_provence()
 )
 
 # st.sidebar.subheader('Luogo')
 location = st.sidebar.selectbox(
     label='digita parte del luogo',
     index=None,
-    options=get_location(prov=provence)
+    options=dal.get_location(prov=provence)
 )
 
 st.sidebar.subheader('Biglietto')
 category = st.sidebar.selectbox(
     label='seleziona una categoria',
     index=None,
-    options=get_category()
+    options=dal.get_category()
 )
 
 
 
 
 # winners = get_winners(category=3, prov='MI')
-winners = get_winners(prov=provence, location=location, category=category)
+winners = dal.get_winners(prov=provence, location=location, category=category)
 st.write(winners.head())
 st.write(len(winners))
 
