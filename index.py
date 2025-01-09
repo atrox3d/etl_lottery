b4f2318dca12d28db6c9358499b6b886880616a1
atrox3d <atrox3d@gmail.com>
2025-01-09 09:56:25 +0100

    add reset button

--- index.py
+++ index.py
@@ -1,8 +1,16 @@
+import logging
 import pandas as pd
 import numpy as np
 import streamlit as st
 
 from dashboard import dal
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s | %(funcName)s | %(message)s'
+)
 
 
 st.title('Analisi lotteria italia')
@@ -17,32 +25,66 @@ st.write(f'''
 st.sidebar.header('Filtri')
 st.sidebar.write('Usare le opzioni per filtrare i dati')
 
+def reset():
+    for el in st.session_state:
+        st.session_state[el] = None
+
+st.sidebar.button('reset filtri',
+    on_click=reset
+)
+
+
 st.sidebar.subheader('Geograficamente')
 prov = st.sidebar.selectbox(
     label='seleziona una provincia',
     index=None,
-    options=dal.get_prov()
+    options=dal.get_prov(),
+    key='prov'
 )
+
 
 # st.sidebar.subheader('Luogo')
 luogo = st.sidebar.selectbox(
     label='digita parte del luogo',
     index=None,
-    options=dal.get_luogo(prov=prov)
+    options=dal.get_luogo(prov=prov),
+    key='luogo'
 )
 
 st.sidebar.subheader('Biglietto')
 categoria = st.sidebar.selectbox(
     label='seleziona una categoria',
     index=None,
-    options=dal.get_categoria()
+    options=dal.get_categoria(),
+    key='categoria'
+)
+
+serie = st.sidebar.selectbox(
+    label='seleziona una serie',
+    index=None,
+    options=dal.get_serie(),
+    key='serie'
+)
+
+numero = st.sidebar.selectbox(
+    label='seleziona una numero',
+    index=None,
+    options=dal.get_numero(serie=serie),
+    key='numero'
 )
 
 
 
 
 # winners = get_winners(category=3, prov='MI')
-winners = dal.get_winners(prov=prov, luogo=luogo, categoria=categoria)
+winners = dal.get_winners(
+    prov=prov, 
+    luogo=luogo, 
+    categoria=categoria,
+    serie=serie,
+    numero=numero
+)
 st.write(winners.head())
 st.write(len(winners))
 
+st.session_state
