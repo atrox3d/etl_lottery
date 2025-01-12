6866f6364058ae28ce72f81220e46914154414d3
atrox3d <atrox3d@gmail.com>
2025-01-12 09:10:20 +0100

    move selectboxed to dashboard.header

---
+++ dashboard/header.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import streamlit as st
+from dashboard import helpers, fixselect, pandasdal as dal
+
+def display_options() -> tuple[bool, bool]:
+    show_count = st.checkbox(
+        label='show count',
+        value=True,
+        # key='link'
+    )
+    show_state = st.checkbox(
+        label='show state',
+        value=True,
+        # key='link'
+    )
+    
+    return show_count, show_state
+
+
+def filter_options() -> bool:
+    st.header('Filtri')
+    st.write('Usare le opzioni per filtrare i dati')
+    
+    link = st.checkbox( label='filtri collegati', value=True, )
+    
+    st.button('reset filtri',
+        on_click=helpers.reset_widgets
+    )
+    
+    return link
+
+
+def geo_filters(df:pd.DataFrame, link:bool) -> tuple[str, str]:
+    prov = st.selectbox(                                                     # PROV
+        label='seleziona una provincia',
+        index=None if fixselect.FIX_INDEX else 0,
+        options=dal.get_prov(df, link, **st.session_state),
+        key='prov',
+        placeholder='Non selezionato'
+    )
+
+    luogo = st.selectbox(                                                   # LUOGO
+        label='digita parte del luogo',
+        index=None,
+        options=dal.get_luogo(df, link, **st.session_state),
+        key='luogo',
+        placeholder='Non selezionato'
+    )
+    
+    return prov, luogo
+
+
+def ticket_filters(df:pd.DataFrame, link:bool) -> tuple[str, str]:
+    serie = st.selectbox(                                                   # SERIE
+        label='seleziona una serie',
+        index=None,
+        options=dal.get_serie(df, link, **st.session_state),
+        key='serie',
+        placeholder='Non selezionato',
+        # on_change=reset_geo
+    )
+
+    numero = st.selectbox(                                                  # NUMERO
+        label='seleziona una numero',
+        index=None,
+        options=dal.get_numero(df, link, **st.session_state),
+        key='numero',
+        placeholder='Non selezionato',
+        # on_change=reset_geo
+    )
+    
+    return serie, numero
+
+
+def prize_filters(df:pd.DataFrame, link:bool) -> tuple[str, str]:
+    categoria = st.selectbox(                                               # CATEGORIA
+        label='seleziona una categoria',
+        index=None,
+        options=dal.get_categoria(df, link, **st.session_state),
+        key='categoria',
+        placeholder='Non selezionato'
+    )
+    
+    premio = st.selectbox(                                                  # PREMIO
+        label='seleziona premio',
+        index=None,
+        options=dal.get_premio(df, link, **st.session_state),
+        key='premio',
+        placeholder='Non selezionato'
+    )
+    
+    return categoria, premio
+
--- index.py
+++ index.py
@@ -4,12 +4,11 @@ import streamlit as st
 from dashboard import pandasdal as dal
 from dashboard import fixselect
 from dashboard import helpers
+from dashboard import header
 
 
 ###############################################################################
-#
 #   setup logging, dataframe, gui fixes
-#
 ###############################################################################
 logger = logging.getLogger(__name__)
 
@@ -22,9 +21,7 @@ df  = dal.get_winners().copy()
 
 helpers.fix_widgets_reload()
 ###############################################################################
-#
 #   title
-#
 ###############################################################################
 st.title('Analisi lotteria italia')
 st.write(f'''
@@ -33,103 +30,32 @@ st.write(f'''
     stato della connessione: {dal.get_connection_status()}
 ''')
 with st.sidebar:
-    show_count = st.checkbox(
-        label='show count',
-        value=True,
-        # key='link'
-    )
-    show_state = st.checkbox(
-        label='show state',
-        value=True,
-        # key='link'
-    )
+    show_count, show_state = header.display_options()
 ###############################################################################
-#
 #   sidebar header filtri
-#
 ###############################################################################
-    st.header('Filtri')
-    st.write('Usare le opzioni per filtrare i dati')
-    
-    link = st.checkbox( label='filtri collegati', value=True, )
-    
-    st.button('reset filtri',
-        on_click=helpers.reset_widgets
-    )
+    link = header.filter_options()
 ###############################################################################
-#
 #   sidebar subheader geo
-#
 ###############################################################################
     st.subheader('Geografia')
     
-    prov = st.selectbox(                                                     # PROV
-        label='seleziona una provincia',
-        index=None if fixselect.FIX_INDEX else 0,
-        options=dal.get_prov(df, link, **st.session_state),
-        key='prov',
-        placeholder='Non selezionato'
-    )
-
-    luogo = st.selectbox(                                                   # LUOGO
-        label='digita parte del luogo',
-        index=None,
-        options=dal.get_luogo(df, link, **st.session_state),
-        key='luogo',
-        placeholder='Non selezionato'
-    )
+    prov, luogo = header.geo_filters(df, link)
 ###############################################################################
-#
 #   sidebar subheader biglietto
-#
 ###############################################################################
     st.subheader('Biglietto')
     
-    serie = st.selectbox(                                                   # SERIE
-        label='seleziona una serie',
-        index=None,
-        options=dal.get_serie(df, link, **st.session_state),
-        key='serie',
-        placeholder='Non selezionato',
-        # on_change=reset_geo
-    )
-
-    numero = st.selectbox(                                                  # NUMERO
-        label='seleziona una numero',
-        index=None,
-        options=dal.get_numero(df, link, **st.session_state),
-        key='numero',
-        placeholder='Non selezionato',
-        # on_change=reset_geo
-    )
+    serie, numero = header.ticket_filters(df, link)
 ###############################################################################
-#
 #   sidebar subheader premio
-#
 ###############################################################################
     st.subheader('Premio')
     
-    categoria = st.selectbox(                                               # CATEGORIA
-        label='seleziona una categoria',
-        index=None,
-        options=dal.get_categoria(df, link, **st.session_state),
-        key='categoria',
-        placeholder='Non selezionato'
-    )
-    
-    premio = st.selectbox(                                                  # PREMIO
-        label='seleziona premio',
-        index=None,
-        options=dal.get_premio(df, link, **st.session_state),
-        key='premio',
-        placeholder='Non selezionato'
-    )
+    categoria, premio = header.prize_filters(df, link)
 ###############################################################################
-#
 #   TABLE
-#
 ###############################################################################
-
 winners = dal.get_winners(**st.session_state)
 
 st.dataframe( winners, hide_index=True )
