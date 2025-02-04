b582436f3cb36d72444f215c3df90fbf8c80cb91
atrox3d <atrox3d@gmail.com>
2025-02-04 12:15:32 +0100

    setup typer main

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
+import typer
 import logging
 
-from dbhelpers.db import get_db_url
-from etl.extract import get_df_from_html
-from etl.load import load_to_mysql
-from dbhelpers.config import build_config
-
 logger = logging.getLogger(__name__)
+
+app = typer.Typer(
+    add_completion  = False,   # disable completion hint
+    no_args_is_help = False   # need to always execute main callback
+)
 
 INPUT_PATH = 'data/in/lotteria.html'
 DB_NAME = 'testing'
 
+@app.command()
+def main():
+    pass
+
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO
-    )
-    logger.info('start etl process')
-    
-    logger.info(f'loading data')
-    winners = get_df_from_html(INPUT_PATH)
-    check_nan = winners.isna().values.any()
-    assert check_nan == False
-    
-    logger.info('loading config')
-    config = build_config(database=DB_NAME)
-    
-    logger.info('creating db URL')
-    db_url = get_db_url(**config)
-    
-    logger.info('loading data to mysql db')
-    load_to_mysql(winners, db_url)
-    
-    logger.info('end etl process')
+    app()
---
+++ main_mysql.py
@@ -0,0 +1,33 @@
+import logging
+
+from dbhelpers.db import get_db_url
+from etl.extract import get_df_from_html
+from etl.load import load_to_mysql
+from dbhelpers.config import build_config
+
+logger = logging.getLogger(__name__)
+
+INPUT_PATH = 'data/in/lotteria.html'
+DB_NAME = 'testing'
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO
+    )
+    logger.info('start etl process')
+    
+    logger.info(f'loading data')
+    winners = get_df_from_html(INPUT_PATH)
+    check_nan = winners.isna().values.any()
+    assert check_nan == False
+    
+    logger.info('loading config')
+    config = build_config(database=DB_NAME)
+    
+    logger.info('creating db URL')
+    db_url = get_db_url(**config)
+    
+    logger.info('loading data to mysql db')
+    load_to_mysql(winners, db_url)
+    
+    logger.info('end etl process')
