0f41e1e28f2025b0ef0285b79b6a4d70a35265a7
atrox3d <atrox3d@gmail.com>
2025-02-05 11:47:25 +0100

    create base querybuilder tests

--- tests/dbhelpers/test_querybuilder.py
+++ tests/dbhelpers/test_querybuilder.py
@@ -1,5 +1,71 @@
-from dbhelpers import querybuilder
+from dbhelpers import querybuilder as qb
 
 
-def test_import():
-    pass
+BIND_PARAM = '?'
+
+def test_simple_select_no_params():
+    
+    query = 'select * from table'
+    sql, params = qb.query_builder(
+        query,
+    )
+    print(f'{sql = }')
+    print(f'{params = }')
+    assert query == sql
+    assert params == []
+
+
+def test_simple_select_one_param():
+    
+    query = 'select * from table'
+    sql, params = qb.query_builder(
+        query,
+        name='bob'
+    )
+    print(f'{sql = }')
+    print(f'{params = }')
+    assert sql == f'{query} WHERE name = {BIND_PARAM}'
+    assert params == ['bob']
+
+
+def test_simple_select_two_params():
+    
+    query = 'select * from table'
+    sql, params = qb.query_builder(
+        query,
+        name='bob',
+        age=10
+    )
+    print(f'{sql = }')
+    print(f'{params = }')
+    assert sql == f'{query} WHERE name = {BIND_PARAM} AND age = {BIND_PARAM}'
+    assert params == ['bob', 10]
+
+
+def test_simple_select_with_one_param_like():
+    
+    query = 'select * from table'
+    sql, params = qb.query_builder(
+        query,
+        name__like='bob',
+    )
+    print(f'{sql = }')
+    print(f'{params = }')
+    assert sql == f'{query} WHERE name like {BIND_PARAM}'
+    assert params == ['%bob%']
+
+
+def test_simple_select_with_one_param_and_one_like():
+    
+    query = 'select * from table'
+    sql, params = qb.query_builder(
+        query,
+        name__like='bob',
+        age=10
+    )
+    print(f'{sql = }')
+    print(f'{params = }')
+    assert sql == f'{query} WHERE name like {BIND_PARAM} AND age = {BIND_PARAM}'
+    assert params == ['%bob%', 10]
+
+
